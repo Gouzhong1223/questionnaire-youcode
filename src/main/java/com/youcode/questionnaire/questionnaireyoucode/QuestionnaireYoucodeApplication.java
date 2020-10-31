73d5cc61d01fab04cefe543def20a1e0153e2a7e
Gouzhong1223 <1162864960@qq.com>
2020-10-31 16:45:02 +0800

    添加配置文件以及 banner.txt

--- src/main/java/com/youcode/questionnaire/questionnaireyoucode/QuestionnaireYoucodeApplication.java
+++ src/main/java/com/youcode/questionnaire/questionnaireyoucode/QuestionnaireYoucodeApplication.java
@@ -3,6 +3,9 @@ package com.youcode.questionnaire.questionnaireyoucode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author qingsong
+ */
 @SpringBootApplication
 public class QuestionnaireYoucodeApplication {
 
