1340a9fb4feb29cf0aa7cd896f9c7456592d5094
Gouzhong1223 <1162864960@qq.com>
2020-10-31 16:13:09 +0800

    初始化项目

---
+++ src/main/java/com/youcode/questionnaire/questionnaireyoucode/QuestionnaireYoucodeApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.questionnaire.questionnaireyoucode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireYoucodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireYoucodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/questionnaire/questionnaireyoucode/QuestionnaireYoucodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.questionnaire.questionnaireyoucode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireYoucodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
