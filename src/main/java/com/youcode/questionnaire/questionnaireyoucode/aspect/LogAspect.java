1352e4245f049e737b25d49beaa07ffe764be955
Gouzhong1223 <1162864960@qq.com>
2020-11-01 13:07:31 +0800

    新增 Aspect 日志拦截

---
+++ src/main/java/com/youcode/questionnaire/questionnaireyoucode/aspect/LogAspect.java
@@ -0,0 +1,78 @@
+package com.youcode.questionnaire.questionnaireyoucode.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description :
+ * @Date : create by QingSong in 2020-04-21 12:30 下午
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.gouzhong1223.cymmtj.aspect
+ * @ProjectName : cymmtj
+ * @Version : 1.0.0
+ */
+@Component
+@Aspect
+public class LogAspect {
+    private final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(* com.youcode.questionnaire.questionnaireyoucode.controller..*(..))")
+    public void log() {
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+        logger.info("Request : {}", requestLog);
+    }
+
+    @After("log()")
+    public void doAfter() {
+    }
+
+    @AfterReturning(returning = "result", pointcut = "log()")
+    public void doAfterRuturn(Object result) {
+        logger.info("Result : {}", result);
+    }
+
+    private class RequestLog {
+        private final String url;
+        private final String ip;
+        private final String classMethod;
+        private final Object[] args;
+
+        public RequestLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+}
